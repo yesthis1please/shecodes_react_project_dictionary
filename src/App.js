b4e8714ae8da4753a9193138bea119a01d1f04a8
yesthis1please <yesthis1please@gmail.com>
2022-06-20 10:05:41 +0200

    Created file structure

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary</h1>
+      </header>
+
+      <footer className="App-footer">
+        This Dictionary App was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/annamaria-bara/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Annamaria Bara
+        </a>{" "}
+        and is open sourced on{" "}
+        <a
+          href="https://github.com/yesthis1please"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
+}
